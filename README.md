19c278090c0bbe4756afe5c663e8fd2b12c273ac
Aakash <71321862+Just5KY@users.noreply.github.com>
2022-05-25 00:18:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# wordpot-docker
+wordpress honeypot dockerized
+
+
+```yml
+version: "3"
+
+networks:
+  wordpot:
+
+services:
+  wordpot:
+    container_name: wordpot
+    image: "justsky/wordpot"
+    restart: unless-stopped
+    networks:
+      - wordpot
+    ports:
+      - "80:80"
+    volumes:
+      - $DOCKERDIR/wordpot/:/opt/wordpot2/log
+```
---
+++ log.py
@@ -0,0 +1,37 @@
+import subprocess as sp
+from concurrent.futures import ThreadPoolExecutor
+
+
+def log_popen_pipe(p, stdfile):
+
+    with open("/log/wordpot.log", "w") as f:
+
+        while p.poll() is None:
+            f.write(stdfile.readline())
+            f.flush()
+
+        # Write the rest from the buffer
+        f.write(stdfile.read())
+
+
+with sp.Popen(
+    [
+        "python3",
+        "wordpot2.py",
+        "--host",
+        "0.0.0.0",
+        "--port",
+        "80",
+        "--title",
+        "QA 20220317",
+    ],
+    stdout=sp.PIPE,
+    stderr=sp.PIPE,
+    text=True,
+) as p:
+
+    with ThreadPoolExecutor(2) as pool:
+        r1 = pool.submit(log_popen_pipe, p, p.stdout)
+        r2 = pool.submit(log_popen_pipe, p, p.stderr)
+        r1.result()
+        r2.result()
